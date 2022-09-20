859b1a0924882136efc2504c693b18cd5ca4827e
rebecatinnoco <104739229+rebecatinnoco@users.noreply.github.com>
2022-09-20 00:19:18 -0400

    commit #2final

--- shopcart/src/Cart.js
+++ shopcart/src/Cart.js
@@ -16,7 +16,7 @@ function ShowCart(props) {
     return (
         <div className="w-150">
             <h2>Your Cart Items</h2>
-            {/* {props.products.map((products) =>  (
+            {props.products.map((products) =>  (
                     <div key={products.id} className="border">
                         <img src={products.img} alt={products.description} width="150"></img>
                         <p className="qty d-inline mx-5 fw-bold">Quantity {products.quantity}</p>
@@ -24,7 +24,15 @@ function ShowCart(props) {
                     </div>
                    )
 
-            )} */}
+            )}
         </div>
     )
 }
+
+// if (prod.quantity > 0) {
+//     <div key={prod.id} className="border">
+//     <img src={prod.img} alt={prod.description} width="150"></img>
+//     <p className="qty d-inline mx-5 fw-bold">Quantity {prod.quantity}</p>
+//     <p className="px-4">{prod.description}</p>
+// </div>
+// }
